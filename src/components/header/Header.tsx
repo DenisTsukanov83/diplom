5136af2ef2c245c754791d81fd97e3cf5cacfa99
Denis Tsukanov <tsukanovdenis83@yandex.ru>
2024-04-23 10:22:31 +0400

    added header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import './Header.scss';
-import location from '../../assets/img/Location.png';
+import location from '../../assets/img/header/Location.png';
+import search from '../../assets/img/header/Search.png';
+import calling from '../../assets/img/header/Calling.png';
 
 function Header() {
     return (
@@ -12,16 +14,23 @@ function Header() {
                 <div className="header-input-loc">
                     <img src={location} alt="Location.png" />
                 </div>
-                <input type="text" />
-                <div className="header-input-loupe">
-                <img src="" alt="" />
+                <input type="text" placeholder='Введите адрес доставки'/>
+                <div className="header-input-search">
+                <img src={search} alt="Search.png" />
                 </div>
             </div>
             <div className="header-contact">
-
+                <div className="header-contact-img">
+                    <img src={calling} alt="Calling.png" />
+                </div>
+                <div className="header-contact-phone">
+                    <div className="header-contact-contacts">Контакты:</div>
+                    <div className="header-contact-number">+7 (917) 510-57-59</div>
+                </div>
             </div>
             <div className="header-basket">
-
+                <div className="header-basket-title">Корзина</div>
+                <div className="header-basket-number">4</div>
             </div>
         </header>
     );
