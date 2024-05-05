90c83d2935f13535b0b9121657750d0839f84ed5
Denis Tsukanov <tsukanovdenis83@yandex.ru>
2024-05-05 15:34:50 +0400

    added button sign in to the basket header

--- src/components/basket/Basket.tsx
+++ src/components/basket/Basket.tsx
@@ -1,12 +1,16 @@
-import React, {FC, useContext} from 'react';
+import React, { FC, useContext } from 'react';
 import './Basket.scss';
 import { Link } from 'react-router-dom';
 
-import HeaderMenu from '../headerMenu/HeaderMenu';
 import Dishes from '../dishes/Dishes';
 import BasketItem from '../basketItem/BasketItem';
 import BasketAddItem from '../basketAddItem/BasketAddItem';
 import Footer from '../footer/Footer';
+
+import location from '../../assets/img/header/Location.png';
+import search from '../../assets/img/header/Search.png';
+import calling from '../../assets/img/header/Calling.png';
+import profile from '../../assets/img/basket/Profile.png';
 
 import { Context } from '../../App';
 
@@ -14,7 +18,7 @@ import { Context } from '../../App';
 
 
 const Basket: FC = () => {
-    const {basketArr, numberOfBasket} = useContext<any>(Context);
+    const { basketArr, numberOfBasket } = useContext<any>(Context);
     let sum = 0;
     basketArr.forEach((el: any) => {
         sum += el.number * el.obj.price;
@@ -23,8 +27,51 @@ const Basket: FC = () => {
 
     return (
         <div className="basket">
-            <HeaderMenu numberOfBasket={numberOfBasket}/>
-            <Dishes/>
+            <div className="basket-menu">
+                <div className="basket-logo">
+                    logos
+                </div>
+                <div className="basket-hamburger">
+                    <div>
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                    </div>
+                    <div>Меню</div>
+                </div>
+                <div className="basket-input">
+                    <div className="basket-input-loc">
+                        <img src={location} alt="Location.png" />
+                    </div>
+                    <input type="text" placeholder='Введите адрес доставки' />
+                    <div className="basket-input-search">
+                        <img src={search} alt="Search.png" />
+                    </div>
+                </div>
+                <div className="basket-contact">
+                    <div className="basket-contact-img">
+                        <img src={calling} alt="Calling.png" />
+                    </div>
+                    <div className="basket-contact-phone">
+                        <div className="basket-contact-contacts">Контакты:</div>
+                        <div className="basket-contact-number">+7 (917) 510-57-59</div>
+                    </div>
+                </div>
+                <button className="basket-signIn">
+                    <div className="basket-signIn-img">
+                        <img src={profile} alt="Profile.png" />
+                    </div>
+                    <div className="basket-signIn-title">Войти</div>
+                </button>
+
+                <Link to={'/basket'} className="no-underline">
+                    <button className="basket-basket">
+                        <div className="basket-basket-title">Корзина</div>
+                        <div className="basket-basket-number">{numberOfBasket}</div>
+                    </button>
+                </Link>
+            </div>
+            <Dishes />
             <div className="basket-header">
                 <Link to={'/diplom'} className="no-underline">
                     <div className="basket-header-return">
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,18 +1,57 @@
-import React, {useContext} from 'react';
+import React, { useContext } from 'react';
+import { Link } from 'react-router-dom';
 import './Header.scss';
 import bg from '../../assets/img/header/main-bg.jpeg';
-import HeaderMenu from '../headerMenu/HeaderMenu';
+import location from '../../assets/img/header/Location.png';
+import search from '../../assets/img/header/Search.png';
+import calling from '../../assets/img/header/Calling.png';
 
 import { Context } from '../../App';
 
 
 
 function Header() {
-    const {numberOfBasket} = useContext<any>(Context);
+    const { numberOfBasket } = useContext<any>(Context);
 
     return (
         <header className="header">
-            <HeaderMenu numberOfBasket={numberOfBasket}/>
+            <div className="header-menu">
+                <div className="header-logo">
+                    logos
+                </div>
+                <div className="header-hamburger">
+                    <div>
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                    </div>
+                    <div>Меню</div>
+                </div>
+                <div className="header-input">
+                    <div className="header-input-loc">
+                        <img src={location} alt="Location.png" />
+                    </div>
+                    <input type="text" placeholder='Введите адрес доставки' />
+                    <div className="header-input-search">
+                        <img src={search} alt="Search.png" />
+                    </div>
+                </div>
+                <div className="header-contact">
+                    <div className="header-contact-img">
+                        <img src={calling} alt="Calling.png" />
+                    </div>
+                    <div className="header-contact-phone">
+                        <div className="header-contact-contacts">Контакты:</div>
+                        <div className="header-contact-number">+7 (917) 510-57-59</div>
+                    </div>
+                </div>
+                <Link to={'/basket'} className="no-underline">
+                    <button className="header-basket">
+                        <div className="header-basket-title">Корзина</div>
+                        <div className="header-basket-number">{numberOfBasket}</div>
+                    </button>
+                </Link>
+            </div>
 
             <div className="header-main">
                 <div className="header-main-bg">
